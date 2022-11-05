af1b3e672808fa01caad37141725fcc15ceff757
leliksey <captainfroody@gmail.com>
2022-11-05 15:48:21 +0200

    category slider

--- assets/js/components/owl.js
+++ assets/js/components/owl.js
@@ -1,6 +1,7 @@
-$('.header-bottom__category').owlCarousel({
+$('.popular__slider').owlCarousel({
     loop:true,
     nav:true,
+    margin:19,
     mouseDrag:true,
     touchDrag: true,
     pullDrag:false,
@@ -12,205 +13,30 @@ $('.header-bottom__category').owlCarousel({
         0:{
             items:3
         },
-        576:{
+        680:{
+            items:4
+        },
+        
+        992:{
             items:5
         },
-        1400:{
+        1300:{
             items:7
+        },
+        1610:{
+            items:9
         }
     }
 });
-$(".header-bottom__category-button").on("click", ()=> {
-    $(".header-bottom__category .owl-next").click();
+$(".popular__slider-nav img:first-child").on("click", ()=> {
+    $(".popular__slider .owl-prev").click();
+});
+$(".popular__slider-nav img:last-child").on("click", ()=> {
+    $(".popular__slider .owl-next").click();
 });
 
 
-$('.banners__slider').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    mouseDrag:true,
-    touchDrag: true,
-    pullDrag:false,
-    freeDrag:false,
-    URLhashListener:false,
-    dots:true,
-    autoplay:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-});
 
-$('.products').owlCarousel({
-    loop:true,
-    margin:15,
-    nav:false,
-    mouseDrag:true,
-    touchDrag: true,
-    pullDrag:false,
-    freeDrag:false,
-    URLhashListener:false,
-    dots:true,
-    autoplay:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        450:{
-            items:2
-        },
-        576:{
-            items:3
-        }
-    }
-});
-
-$('.popular__tab-item').owlCarousel({
-    loop:true,
-    margin:15,
-    nav:false,
-    mouseDrag:true,
-    touchDrag: true,
-    pullDrag:false,
-    freeDrag:false,
-    URLhashListener:false,
-    dots:true,
-    autoplay:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        450:{
-            items:2
-        },
-        576:{
-            items:3
-        }
-    }
-});
-// $('.popular__tab-item').slick({
-//     infinite: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 3,
-//     variableWidth: true,
-//     responsive: [
-//     {
-//       breakpoint: 450,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         infinite: true,
-//         dots: true
-//       }
-//     }
-//   ]
-//   });
-$('.recommended__slider').owlCarousel({
-    loop:true,
-    margin:30,
-    nav:false,
-    mouseDrag:true,
-    touchDrag: true,
-    pullDrag:false,
-    freeDrag:false,
-    URLhashListener:false,
-    dots:true,
-    autoplay:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        450:{
-            items:2
-        },
-        670:{
-            items:3
-        },
-        992:{
-            items:4
-        }
-    }
-});
-
-$('.news__content').owlCarousel({
-    loop:true,
-    margin:15,
-    nav:true,
-    mouseDrag:true,
-    touchDrag: true,
-    pullDrag:false,
-    freeDrag:false,
-    URLhashListener:false,
-    dots:false,
-    autoplay:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        450:{
-            items:1
-        },
-        768:{
-            items:1
-        }
-    }
-});
-$('.news__list_mobile').owlCarousel({
-    loop:true,
-    margin:15,
-    nav:true,
-    mouseDrag:true,
-    touchDrag: true,
-    pullDrag:false,
-    freeDrag:false,
-    URLhashListener:false,
-    dots:true,
-    autoplay:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        450:{
-            items:1
-        },
-        768:{
-            items:1
-        }
-    }
-});
-$('.reviews__list').owlCarousel({
-    loop:true,
-    margin:15,
-    items:3,
-    nav:true,
-    mouseDrag:true,
-    touchDrag: true,
-    pullDrag:false,
-    freeDrag:false,
-    URLhashListener:false,
-    dots:true,
-    autoplay:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        768:{
-            items:2,
-            margin:30
-        },
-        992:{
-            items:3
-        }
-    }
-});
 
 $('.slider-category').slick({
     slidesToShow: 1,
