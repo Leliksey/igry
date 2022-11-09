182695283a311e0edf8886855834778010b0fdb3
leliksey <captainfroody@gmail.com>
2022-11-09 18:38:47 +0200

    category final

--- assets/js/components/oneMoreScript.js
+++ assets/js/components/oneMoreScript.js
@@ -1,19 +1,46 @@
 
 
 
-
+//burger
 $(".header__burger").click(function() {
     $(".header__mobile_icon img").toggleClass("dominant");
     $(".category__menu").toggleClass("active");
     $("body").toggleClass("category__menu_open");
 });
+
+//search
 $(".header__mobile_search").click(function() {
-    $("body").toggleClass("popup__search_open");
+    $("body").addClass("popup__active");
 });
+
+//???
 $(".main__center-pages__item").click(function() {
     $(this).parent().find(".active").removeClass("active");
     $(this).addClass("active");
+});
+
+//popup 
+$(".header__mobile_search").on("click", function() {
+    if ($(window).width() < 768) {
+        $(".popup__search").addClass("active");
+        $("body").append("<div class='popup__open'></div>")
+    }
+});
+window.addEventListener("click", function (e) {
+    if($(e.target).hasClass("popup__open")) {
+    $(".popup__open").remove();
+    $(".popup").removeClass("active");
+    $("body").removeClass("popup__active");
+    }
+});
+
+$(".comment").click(function() {
+    $("body").addClass("popup__active");
+    $(".popup-comment").addClass("active");
+    $("body").append("<div class='popup__open'></div>");
+    
 })
+
 
 $(document).ready(function() {
     if ($(window).width() < 768) {
@@ -26,6 +53,12 @@ $(document).ready(function() {
             $(this).parent().parent().parent().find(".main__center-bottom").slideToggle();
             $(this).parent().parent().find(".main__center-category__games").toggleClass("close");
             $(this).parent().parent().find(".main__center-category__game:nth-child(n+4)").slideToggle();
+        })
+        $(".categoryPage .category__name").click(function() { 
+            // $(this).parent().parent().find(".categoryPage__center_mobile_wrapper").slideToggle();
+            $(".ukr").slideToggle();
+            $(".categoryPage").toggleClass("active");
+            $(this).parent().parent().toggleClass("close");
         })
     } else if ($(window).width() > 768) {
         $(".main__center").last().removeClass("close");
