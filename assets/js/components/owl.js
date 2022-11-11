ceed1f43e6f8c098f36bc6d2c8d3420b4547a3db
leliksey <captainfroody@gmail.com>
2022-11-11 17:19:44 +0200

    product preFinal

--- assets/js/components/owl.js
+++ assets/js/components/owl.js
@@ -1,142 +1,144 @@
 //owl2row plugin
 if ($(window).width() < 768) {
-(function ($, window, document, undefined) {
-    Owl2row = function (scope) {
-        this.owl = scope;
-        this.owl.options = $.extend({}, Owl2row.Defaults, this.owl.options);
-        //link callback events with owl carousel here
+    (function ($, window, document, undefined) {
+        Owl2row = function (scope) {
+            this.owl = scope;
+            this.owl.options = $.extend({}, Owl2row.Defaults, this.owl.options);
+            //link callback events with owl carousel here
 
-        this.handlers = {
-            'initialize.owl.carousel': $.proxy(function (e) {
-                if (this.owl.settings.owl2row) {
-                    this.build2row(this);
-                }
-            }, this)
+            this.handlers = {
+                'initialize.owl.carousel': $.proxy(function (e) {
+                    if (this.owl.settings.owl2row) {
+                        this.build2row(this);
+                    }
+                }, this)
+            };
+
+            this.owl.$element.on(this.handlers);
         };
 
-        this.owl.$element.on(this.handlers);
-    };
+        Owl2row.Defaults = {
+            owl2row: false,
+            owl2rowTarget: 'item',
+            owl2rowContainer: 'owl2row-item',
+            owl2rowDirection: 'utd' // ltr
+        };
 
-    Owl2row.Defaults = {
-        owl2row: false,
-        owl2rowTarget: 'item',
-        owl2rowContainer: 'owl2row-item',
-        owl2rowDirection: 'utd' // ltr
-    };
+        //mehtods:
+        Owl2row.prototype.build2row = function(thisScope){
+        
+            var carousel = $(thisScope.owl.$element);
+            var carouselItems = carousel.find('.' + thisScope.owl.options.owl2rowTarget);
 
-    //mehtods:
-    Owl2row.prototype.build2row = function(thisScope){
-    
-        var carousel = $(thisScope.owl.$element);
-        var carouselItems = carousel.find('.' + thisScope.owl.options.owl2rowTarget);
+            var aEvenElements = [];
+            var aOddElements = [];
 
-        var aEvenElements = [];
-        var aOddElements = [];
+            $.each(carouselItems, function (index, item) {
+                if ( index % 2 === 0 ) {
+                    aEvenElements.push(item);
+                } else {
+                    aOddElements.push(item);
+                }
+            });
 
-        $.each(carouselItems, function (index, item) {
-            if ( index % 2 === 0 ) {
-                aEvenElements.push(item);
-            } else {
-                aOddElements.push(item);
+            carousel.empty();
+
+            switch (thisScope.owl.options.owl2rowDirection) {
+                case 'ltr':
+                    thisScope.leftToright(thisScope, carousel, carouselItems);
+                    break;
+
+                default :
+                    thisScope.upTodown(thisScope, aEvenElements, aOddElements, carousel);
             }
-        });
 
-        carousel.empty();
+        };
 
-        switch (thisScope.owl.options.owl2rowDirection) {
-            case 'ltr':
-                thisScope.leftToright(thisScope, carousel, carouselItems);
-                break;
+        Owl2row.prototype.leftToright = function(thisScope, carousel, carouselItems){
 
-            default :
-                thisScope.upTodown(thisScope, aEvenElements, aOddElements, carousel);
-        }
+            var o2wContainerClass = thisScope.owl.options.owl2rowContainer;
+            var owlMargin = thisScope.owl.options.margin;
 
-    };
+            var carouselItemsLength = carouselItems.length;
 
-    Owl2row.prototype.leftToright = function(thisScope, carousel, carouselItems){
+            var firsArr = [];
+            var secondArr = [];
 
-        var o2wContainerClass = thisScope.owl.options.owl2rowContainer;
-        var owlMargin = thisScope.owl.options.margin;
+            //console.log(carouselItemsLength);
 
-        var carouselItemsLength = carouselItems.length;
-
-        var firsArr = [];
-        var secondArr = [];
-
-        //console.log(carouselItemsLength);
-
-        if (carouselItemsLength %2 === 1) {
-            carouselItemsLength = ((carouselItemsLength - 1)/2) + 1;
-        } else {
-            carouselItemsLength = carouselItemsLength/2;
-        }
-
-        //console.log(carouselItemsLength);
-
-        $.each(carouselItems, function (index, item) {
-
-
-            if (index < carouselItemsLength) {
-                firsArr.push(item);
+            if (carouselItemsLength %2 === 1) {
+                carouselItemsLength = ((carouselItemsLength - 1)/2) + 1;
             } else {
-                secondArr.push(item);
+                carouselItemsLength = carouselItemsLength/2;
             }
-        });
 
-        $.each(firsArr, function (index, item) {
-            var rowContainer = $('<div class="' + o2wContainerClass + '"/>');
+            //console.log(carouselItemsLength);
 
-            var firstRowElement = firsArr[index];
-                firstRowElement.style.marginBottom = owlMargin + 'px';
+            $.each(carouselItems, function (index, item) {
 
-            rowContainer
-                .append(firstRowElement)
-                .append(secondArr[index]);
 
-            carousel.append(rowContainer);
-        });
+                if (index < carouselItemsLength) {
+                    firsArr.push(item);
+                } else {
+                    secondArr.push(item);
+                }
+            });
 
-    };
+            $.each(firsArr, function (index, item) {
+                var rowContainer = $('<div class="' + o2wContainerClass + '"/>');
 
-    Owl2row.prototype.upTodown = function(thisScope, aEvenElements, aOddElements, carousel){
+                var firstRowElement = firsArr[index];
+                    firstRowElement.style.marginBottom = owlMargin + 'px';
 
-        var o2wContainerClass = thisScope.owl.options.owl2rowContainer;
-        var owlMargin = thisScope.owl.options.margin;
+                rowContainer
+                    .append(firstRowElement)
+                    .append(secondArr[index]);
 
-        $.each(aEvenElements, function (index, item) {
+                carousel.append(rowContainer);
+            });
 
-            var rowContainer = $('<div class="' + o2wContainerClass + '"/>');
-            var evenElement = aEvenElements[index];
+        };
 
-            evenElement.style.marginBottom = owlMargin + 'px';
+        Owl2row.prototype.upTodown = function(thisScope, aEvenElements, aOddElements, carousel){
 
-            rowContainer
-                .append(evenElement)
-                .append(aOddElements[index]);
+            var o2wContainerClass = thisScope.owl.options.owl2rowContainer;
+            var owlMargin = thisScope.owl.options.margin;
 
-            carousel.append(rowContainer);
-        });
-    };
+            $.each(aEvenElements, function (index, item) {
 
-    /**
-     * Destroys the plugin.
-     */
-    Owl2row.prototype.destroy = function() {
-        var handler, property;
+                var rowContainer = $('<div class="' + o2wContainerClass + '"/>');
+                var evenElement = aEvenElements[index];
 
-        for (handler in this.handlers) {
-            this.owl.dom.$el.off(handler, this.handlers[handler]);
-        }
-        for (property in Object.getOwnPropertyNames(this)) {
-            typeof this[property] !== 'function' && (this[property] = null);
-        }
-    };
+                evenElement.style.marginBottom = owlMargin + 'px';
 
-    $.fn.owlCarousel.Constructor.Plugins['owl2row'] = Owl2row;
-})( window.Zepto || window.jQuery, window,  document );
-//end of owl2row plugin
+                rowContainer
+                    .append(evenElement)
+                    .append(aOddElements[index]);
+
+                carousel.append(rowContainer);
+            });
+        };
+
+        /**
+         * Destroys the plugin.
+         */
+        Owl2row.prototype.destroy = function() {
+            var handler, property;
+
+            for (handler in this.handlers) {
+                this.owl.dom.$el.off(handler, this.handlers[handler]);
+            }
+            for (property in Object.getOwnPropertyNames(this)) {
+                typeof this[property] !== 'function' && (this[property] = null);
+            }
+        };
+
+        $.fn.owlCarousel.Constructor.Plugins['owl2row'] = Owl2row;
+    })( window.Zepto || window.jQuery, window,  document );
+    //end of owl2row plugin
 }
+///
+$(document).ready( ()=> {
 $('.popular__slider').owlCarousel({
     loop:true,
     nav:true,
@@ -173,7 +175,7 @@ $(".popular__slider-nav img:first-child").on("click", ()=> {
 $(".popular__slider-nav img:last-child").on("click", ()=> {
     $(".popular__slider .owl-next").click();
 });
-
+///
 $('.category__slider-items').owlCarousel({
     loop:true,
     nav:true,
@@ -214,18 +216,44 @@ $(".category__slider-nav img:first-child").on("click", ()=> {
 $(".category__slider-nav img:last-child").on("click", ()=> {
     $(".category__slider .owl-next").click();
 });
+///
+// $('.productPage__slider-items').owlCarousel({
+//     loop:true,
+//     nav:true,
+//     margin:19,
+//     mouseDrag:true,
+//     touchDrag: true,
+//     pullDrag:false,
+//     freeDrag:false,
+//     URLhashListener:false,
+//     dots:true,
+//     autoplay:false,
+//     owl2row: true,
+//     items:1,
+// });
+// $(".productPage__slider-nav img:first-child").on("click", ()=> {
+//     $(".productPage__slider .owl-prev").click();
+// });
+// $(".productPage__slider-nav img:last-child").on("click", ()=> {
+//     $(".productPage__slider .owl-next").click();
+// });
 
 
+})
 
-
-// $('.slider-category').slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     arrows: false,
-//     infinite: true,
-//     fade: false,
-//     asNavFor: '.slider-category-nav'
-//   });
+$('.productPage__slider-items').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: true,
+    fade: false,
+  });
+  $(".productPage__slider-nav img:first-child").on("click", ()=> {
+    $(".productPage__slider .slick-prev").click();
+});
+$(".productPage__slider-nav img:last-child").on("click", ()=> {
+    $(".productPage__slider .slick-next").click();
+});
 //   $('.slider-category-nav').slick({
 //     slidesToShow: 6,
 //     slidesToScroll: 1,
